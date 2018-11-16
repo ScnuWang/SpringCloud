a7cf25c9ea996f77fcb3d6ea7dfb74172a147ee1
ScnuWang <scnu_wang@163.com>
2018-11-16 11:55:07 +0800

    新增Zuul网关路由

---
+++ springtravel-eureka-client-zuul/src/main/java/work/morem/springtraveleurekaclientzuul/SpringtravelEurekaClientZuulApplication.java
@@ -0,0 +1,18 @@
+package work.morem.springtraveleurekaclientzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class SpringtravelEurekaClientZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringtravelEurekaClientZuulApplication.class, args);
+    }
+}
