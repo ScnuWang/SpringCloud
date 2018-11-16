8b0e23526e0276959c8e5d33d75fd60ccfe5f6ea
ScnuWang <scnu_wang@163.com>
2018-11-16 16:03:36 +0800

    新增服务链路追踪

---
+++ springtravel-zipkin-client-hi/src/test/java/work/morem/springtravelzipkinclienthi/SpringtravelZipkinClientHiApplicationTests.java
@@ -0,0 +1,16 @@
+package work.morem.springtravelzipkinclienthi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringtravelZipkinClientHiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ springtravel-zipkin-client-miya/src/test/java/work/morem/springtravelzipkinclientmiya/SpringtravelZipkinClientMiyaApplicationTests.java
@@ -0,0 +1,16 @@
+package work.morem.springtravelzipkinclientmiya;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringtravelZipkinClientMiyaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
