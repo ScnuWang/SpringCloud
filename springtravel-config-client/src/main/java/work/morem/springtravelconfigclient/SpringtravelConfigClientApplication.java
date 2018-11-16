3c4290b52cdba44ba4d03b61d721d7b638a2ca24
ScnuWang <scnu_wang@163.com>
2018-11-16 14:37:32 +0800

    新增高可用的分布式配置中心

--- springtravel-config-client/src/main/java/work/morem/springtravelconfigclient/SpringtravelConfigClientApplication.java
+++ springtravel-config-client/src/main/java/work/morem/springtravelconfigclient/SpringtravelConfigClientApplication.java
@@ -3,11 +3,15 @@ package work.morem.springtravelconfigclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
+@RefreshScope
 public class SpringtravelConfigClientApplication {
 
     public static void main(String[] args) {
